0376397b82524a7143cd3a0fab961f024c2d5b33
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-03-19 15:26:44 -0300

    FEAT books resolver

---
+++ src/app/core/books-resolver.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { Book } from "app/models/book";
+import { BookTrackerError } from "app/models/bookTrackerError";
+import { DataService } from "./data.service";
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class BooksResolverService
+  implements Resolve<Book[] | BookTrackerError>
+{
+  constructor(private dataService: DataService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Book[] | BookTrackerError> {
+    return this.dataService.getAllBooks().pipe(catchError((err) => of(err)));
+  }
+}
